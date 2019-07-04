60b372a2a0b3987fe617f0440ded4bdd179c7675
Emre Kunt <NEBOGames>
2019-07-04 15:33:20 +0300

    home & nav component added

---
+++ DatingApp-SPA/src/app/_components/nav/nav.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/_services/Auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('succesfully logged in.');
+    }, error => {
+      console.log('failed to login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
+
+
+
+}
