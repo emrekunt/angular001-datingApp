f9c6bf99dc3fa8b8c7d6dc9c1d83d656bee6e693
Emre Kunt <NEBOGames>
2019-07-05 11:28:11 +0300

    error handling added.

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,8 @@ import { NavComponent } from './_components/nav/nav.component';
 import { AuthService } from './_services/Auth.service';
 import { HomeComponent } from './_components/home/home.component';
 import { RegisterComponent } from './_components/register/register.component';
+import { ErrorInterceptor } from './_services/error.interceptor';
+
 
 @NgModule({
    declarations: [
@@ -24,7 +26,12 @@ import { RegisterComponent } from './_components/register/register.component';
       FormsModule
    ],
    providers: [
-      AuthService
+      AuthService,
+      {
+         provide: HTTP_INTERCEPTORS,
+         useClass: ErrorInterceptor,
+         multi: true
+       },
    ],
    bootstrap: [
       AppComponent
